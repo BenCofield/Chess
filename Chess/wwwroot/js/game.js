58da62bcaa038d4a32e5e8a18abad1e1ca1fccdd
Ben Cofield <benrcofield@gmail.com>
2022-12-20 02:51:08 -0500

    20 dec

--- Chess/wwwroot/js/game.js
+++ Chess/wwwroot/js/game.js
@@ -2,64 +2,6 @@
 
 console.log("Script Loaded");
 
-const BoardState = [[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0]];
-
-class Piece {
-
-    width = 64;
-    height = 64;
-
-    constructor(color, xPos, yPos) {
-
-        this.color = color;
-        this.x = xPos;
-        this.y = yPos;
-    }
-    get img() {
-        let image = document.createElement('img');
-        let src = `/lib/img/${this.type}_${this.color}.png`;
-        image.setAttribute('src', src);
-        return image;
-    }
-
-    MoveSpace(space) {
-        this.x = space.x;
-        this.y = space.y;
-    }
-}
-
-class Pawn extends Piece {
-    type = "pawn";
-}
-
-class Rook extends Piece {
-    type = "rook";
-}
-
-class Knight extends Piece {
-    type = "knight";
-}
-
-class Bishop extends Piece {
-    type = "bishop";
-}
-
-class King extends Piece {
-    type = "king";
-    
-}
-
-class Queen extends Piece {
-    type = "queen";
-}
-
 //unused atm
 const WhitePieces = [];
 const BlackPieces = [];
@@ -67,97 +9,17 @@ const BlackPieces = [];
 const BlackCaptured = [];
 const WhiteCaptured = [];
 
-
-var chessBoardView = {
-
-    canvas: document.createElement("canvas"),
-    begin: function () {
-        this.canvas.width = 512;
-        this.canvas.height = 512;
-        this.canvas.style.backgroundImage = "url('/lib/img/board.png')";
-        this.canvas.style.backgroundSize = "512px 512px";
-
-        this.context = this.canvas.getContext("2d");
-        document.getElementById("gameArea").appendChild(this.canvas);
-
-        this.canvas.addEventListener("click", function (event) {
-            var space = getSpaceClicked(chessBoardView.canvas, event);
-            console.log(space.x, space.y);
-
-            if (selectedPiece === 0) {
-                SelectPiece(space);
-            }
-            else {
-                MovePiece(selectedPiece, space);
-                chessBoardView.UpdateView();
-            }
-        }, true);
-    },
-    //end begin function
-
-    Clear: function () {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    },
-
-    UpdateView: function () {
-        this.Clear();
-
-        BoardState.forEach(row => row.forEach(piece => this.DrawPiece(piece)));
-
-        console.log("Updated View");
-    },
-
-    DrawPiece: function (piece) {
-        if (piece === 0) return;
-        this.context.drawImage(piece.img, (piece.x * 64), (piece.y * 64), 64, 64);
-    }
-}
-
-function DrawRectButton(ctx, boardSpace) {
-    ctx.fillStyle = "rgba(255, 255, 0, 0.5)";
-    ctx.fillRect((boardSpace.x * 64), (boardSpace.y * 64), 64, 64);
-}
-
-//return an object representing board space in format (x, y)
-function getSpaceClicked(canvas, event) {
-    let rect = {
-        left: canvas.offsetLeft,
-        top: canvas.offsetTop,
-    };
-
-    return {
-        x: ((event.clientX - rect.left) / 64) | 0,
-        y: ((event.clientY - rect.top) / 64) | 0
-    };
-}
-
-var selectedPiece = 0;
-function SelectPiece(space) {
-    if (BoardState[space.x][space.y] === 0) return;
-
-    else {
-        DrawRectButton(chessBoardView.context, space);
-        selectedPiece = BoardState[space.x][space.y];
-        console.log("Piece selected: " + selectedPiece.x + ", " + selectedPiece.y);
-    }
-}
-
-function MovePiece(piece, space) {
-    BoardState[piece.x][piece.y] = 0;
-    piece.MoveSpace(space);
-    BoardState[space.x][space.y] = piece;
-    console.log("Piece moved to: " + piece.x + " " + piece.y);
-    selectedPiece = 0;
-}
-
 function StartGame() {
+
     InitBoard();
     chessBoardView.begin();
     console.log("Started Game");
     chessBoardView.UpdateView();
+
 }
 
 function InitBoard() {
+
     let color = "black";
 
     //Pawns
@@ -190,5 +52,185 @@ function InitBoard() {
     BoardState[3][0] = new King("black", 3, 0);
     BoardState[4][7] = new King("white", 4, 7);
 }
+
+const BoardState = [[0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0]];
+
+var chessBoardView = {
+
+    canvas: document.createElement("canvas"),
+    begin: function () {
+        this.canvas.width = 512;
+        this.canvas.height = 512;
+        this.canvas.style.backgroundImage = "url('/lib/img/board.png')";
+        this.canvas.style.backgroundSize = "512px 512px";
+
+        this.context = this.canvas.getContext("2d");
+        document.getElementById("gameArea").appendChild(this.canvas);
+
+        this.canvas.addEventListener("click", (event) => clickAction(event), true);
+    },
+    //end begin function
+
+    Clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+
+    UpdateView: function () {
+        this.Clear();
+
+        BoardState.forEach(row => row.forEach(piece => this.DrawPiece(piece)));
+
+        console.log("Updated View");
+    },
+
+    DrawPiece: function (piece) {
+        if (piece === 0) return;
+
+        this.context.drawImage(piece.img, (piece.x * 64), (piece.y * 64), 64, 64);
+    },
+
+    DrawRectButton: function (boardSpace) {
+        this.context.fillStyle = "rgba(255, 255, 0, 0.5)";
+
+        this.context.fillRect((boardSpace.x * 64), (boardSpace.y * 64), 64, 64);
+    }
+}
+
+class Piece {
+
+    width = 64;
+    height = 64;
+
+    possibleMoves = [];
+
+    constructor(color, xPos, yPos) {
+        this.color = color;
+        this.x = xPos;
+        this.y = yPos;
+    }
+
+    get img() {
+        let image = document.createElement('img');
+        let src = `/lib/img/${this.type}_${this.color}.png`;
+        image.setAttribute('src', src);
+        return image;
+    }
+
+    MoveSpace(space) {
+        this.x = space.x;
+        this.y = space.y;
+    }
+}
+
+class Pawn extends Piece {
+    type = "pawn";
+    
+    moveCount = 0;
+
+    getPossibleMoves() {
+        const list = [];
+
+        let s = {
+            x: this.x,
+            y: this.y + 1
+        };
+
+        list.push(s);
+
+        if (this.moveCount === 0) {
+
+            let s = {
+                x: this.x,
+                y: this.y + 2,
+            };
+
+            list.push(s);
+        }
+
+        list.forEach(s => console.log(s.x, s.y));
+
+        this.possibleMoves = list;
+    }
+}
+
+class Rook extends Piece {
+    type = "rook";
+}
+
+class Knight extends Piece {
+    type = "knight";
+}
+
+class Bishop extends Piece {
+    type = "bishop";
+}
+
+class King extends Piece {
+    type = "king";
+    
+}
+
+class Queen extends Piece {
+    type = "queen";
+}
+
+
+function getSpaceClicked(canvas, event) {
+    let rect = {
+        left: canvas.offsetLeft,
+        top: canvas.offsetTop,
+    };
+
+    return {
+        x: ((event.clientX - rect.left) / 64) | 0,
+        y: ((event.clientY - rect.top) / 64) | 0
+    };
+}
+
+function SelectPiece(space) {
+
+    if (BoardState[space.x][space.y] === 0) return;
+
+    else {
+        selectedPiece = BoardState[space.x][space.y];
+        chessBoardView.DrawRectButton(space);
+
+        selectedPiece.getPossibleMoves();
+        selectedPiece.possibleMoves.forEach(sp => chessBoardView.DrawRectButton(sp));
+    }
+}
+
+function MovePiece(piece, space) {
+
+    BoardState[piece.x][piece.y] = 0;
+    piece.MoveSpace(space);
+    BoardState[space.x][space.y] = piece;
+    selectedPiece = 0;
+
+}
+
+function clickAction(event) {
+
+    var space = getSpaceClicked(chessBoardView.canvas, event);
+    console.log(space.x, space.y);
+
+    if (selectedPiece === 0) {
+        SelectPiece(space);
+    }
+
+    else {
+        MovePiece(selectedPiece, space);
+        chessBoardView.UpdateView();
+    }
+}
+
+var selectedPiece = 0;
 
 StartGame();
