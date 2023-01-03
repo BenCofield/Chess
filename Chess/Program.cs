3da467b302deed3cde089aca13e0ef70d1aa303f
Ben Cofield <benrcofield@gmail.com>
2023-01-02 23:21:47 -0500

    Added Azure Database

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Authentication;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("AzureDb");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
